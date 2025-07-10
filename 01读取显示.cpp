21342efa3613950e18e10a0e62896e5c9443ce4b
zhz77 <1099897459@qq.com>
2025-07-10 18:03:11 +0800

    0710add

--- 01读取显示.cpp
+++ 01读取显示.cpp
@@ -6,10 +6,16 @@ using namespace std;
 
 
 
-int main()
+int main01()
 {
 	
+	string path = "./data/lenna_img.png";
+	Mat img = imread(path, IMREAD_GRAYSCALE);
 
+	namedWindow("img_show", WINDOW_FREERATIO);
+	imshow("img_show", img);
+	waitKey(0);
+	destroyAllWindows();
 
 	system("pause");
 	return 0;
---
+++ 02色彩空间转换.cpp
@@ -0,0 +1,18 @@
+﻿// 文件名：02色彩空间转换
+// 创建时间：2025/7/10 18:02:01
+
+#include <iostream>
+
+using namespace std;
+
+/*
+	
+*/
+
+int main()
+{
+
+
+	system("pause");
+	return 0;
+}
